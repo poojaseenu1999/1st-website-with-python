979cc07e5539e96e656d951415235e4d82ecc035
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-16 02:00:20 +0530

    second commit

--- webapp.py
+++ webapp.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<center><h1>WELCOME TO PYTHON WEBSITE.<br>never give up !! great things take time !!!</h1></center>"
+    return "<center><h1>WELCOME TO PYTHON WEBSITE.</h1><br><h4>never give up !! great things take time !!!</h4></center>"
 if(__name__) == "__main__":
     app.run()
